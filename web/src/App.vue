6be3c94ee762fccea3579f5fc943a601c0fa9d50
gengogo5 <gen0908@gmail.com>
2019-06-13 01:06:16 +0900

    HelloControllerへのGETリクエスト

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,16 +1,39 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <h1>{{msg}}</h1>
+    <h1 v-if="msg.length > 0">
+      {{msg}}
+    </h1>
+    <p v-else>
+      no text
+    </p>
+    <input type="text" v-model="msg">
+    <button @click="hello()">hello</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
-      msg: 'Hello World!'
+      msg: 'helloworld!'
     }
+  },
+  methods: {
+    hello () {
+      axios.get('http://localhost:8080/hello')
+        .then(response => {
+          this.msg = response.data
+       })
+        .catch(err => {
+         this.msg = err;
+      })
+    }
+  },
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8',
+    'Access-Control-Allow-Origin': '*',
   }
 }
 </script>
