65849460cfbc498fe636ceaff752b8d6b46d2c67
gengogo5 <gen0908@gmail.com>
2019-06-16 00:33:46 +0900

    起動時にH2に初期レコードを登録する

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TODO(id, title, description, finished) VALUES (1, '食材を買う', 'にんじん、じゃがいも、カレールー', false);
+INSERT INTO TODO(id, title, description, finished) VALUES (2, 'カレーを作る', '野菜は大きめに切る', false);
+INSERT INTO TODO(id, title, description, finished) VALUES (3, 'カレーを食べる', '食べる前には手を洗う', false);
+INSERT INTO TODO(id, title, description, finished) VALUES (4, '片付ける', '食器と調理器具を綺麗に', false);
